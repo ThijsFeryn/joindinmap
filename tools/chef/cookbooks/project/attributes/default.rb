508aaf254ecec6c82078cd604c498b61efbf0642
Thijs Feryn <thijs@feryn.eu>
2012-11-04 14:26:37 +0100

    Simplified the file structure

--- tools/chef/cookbooks/project/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-default[:project][:hostname] = "joindin.feryn.dev"
-default[:project][:ip] = "12.12.12.111"
-default[:project][:approot] = "/home/data"
-default[:project][:webroot] = "/home/data/public"
