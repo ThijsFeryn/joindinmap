da7164c094ef08db1813e7b41530b4cf7f07832a
Thijs Feryn <thijs@feryn.eu>
2012-11-04 14:29:59 +0100

    Removing deploy.sh

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-echo "Deploying to production";
-rsync -av --delete --exclude=".git" --exclude=".gitignore" --exclude="deploy.sh" --exclude=".idea" ./ root@joindin.feryn.eu:/var/www/joindin.feryn.eu/
